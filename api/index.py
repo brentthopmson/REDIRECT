18544fb9079aa56b43e6a1432b292058ffbee075
robertlugana <brentthopmson@gmail.com>
2025-01-07 08:19:50 -0600

    UPP

--- api/index.py
+++ api/index.py
@@ -12,8 +12,8 @@ def get_redirect_url(path):
         return 'https://secure-sharing.vercel.app/archive4'
     elif path == '/archive4':
         return 'https://secure-sharing.vercel.app/archive5'
-    elif path == '/archive578':
-        return 'https://infield-adobe.vercel.app/' # B10 Adobe
+    elif path == '/archive-58578764':
+        return 'https://auth-microserviceshare.vercel.app/' # B10 TRUE
     elif path == '/archive6':
         return 'https://secure-sharing.vercel.app/archive7'
     elif path == '/archive7456':
